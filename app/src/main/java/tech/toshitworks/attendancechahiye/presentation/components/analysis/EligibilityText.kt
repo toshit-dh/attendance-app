3f5de5cd018413266c29117238e9fc3d98f716b7
toshit-dh <extratdh@gmail.com>
2024-12-19 19:01:26 +0530

    Changed EligibilityText Ui

--- app/src/main/java/tech/toshitworks/attendancechahiye/presentation/components/analysis/EligibilityText.kt
+++ app/src/main/java/tech/toshitworks/attendancechahiye/presentation/components/analysis/EligibilityText.kt
@@ -1,74 +1,110 @@
 package tech.toshitworks.attendancechahiye.presentation.components.analysis
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import tech.toshitworks.attendancechahiye.data.dto.EligibilityData
 
 @Composable
 fun EligibilityAnalysis(
-    eligibilityOfMidterm: Pair<Pair<Int, Int>, Boolean>?,
-    eligibilityOfEndSem: Pair<Pair<Int, Int>, Boolean>?
+    eligibilityOfMidterm: EligibilityData?,
+    eligibilityOfEndSem: EligibilityData?
 ) {
-    val mtDateNotProvided = eligibilityOfMidterm == null
-    val endSemDateNotProvided = eligibilityOfEndSem == null
-    val eligibleMT: Boolean? = eligibilityOfMidterm?.second
-    val mtEligibleText = if (mtDateNotProvided) {
-        "No Date provided"
-    } else if (eligibleMT == true) "Eligible" else "Not Eligible"
-    val eligibleEndSem: Boolean? = eligibilityOfEndSem?.second
-    val endSemEligibleText = if (endSemDateNotProvided) {
-        "No Date provided"
-    } else if (eligibleEndSem == true) "Eligible" else "Not Eligible"
-    Column {
+    val midTermDateProvided = eligibilityOfMidterm != null
+    val endSemDateProvided = eligibilityOfEndSem != null
+
+    Row (
+        modifier = Modifier
+            .fillMaxSize(),
+    ){
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
+                .weight(1f)
+                .padding(5.dp)
         ) {
             Text(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                text = "Mid Term: $mtEligibleText",
+                modifier = Modifier.fillMaxWidth(),
+                text = "Mid Term",
                 textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Bold
-            )
-            if (!mtDateNotProvided) {
-                if (eligibleMT == true) {
-                    Text(
-                        text = "Bunk Lectures: ${eligibilityOfMidterm.first.first}",
-                    )
-                }
-                Text(
-                    text = "More Lectures: ${eligibilityOfMidterm!!.first.second}",
+                style = MaterialTheme.typography.titleMedium.copy(
+                    fontWeight = FontWeight.Bold
                 )
-            }
+            )
+            if (midTermDateProvided)
+                MidTermText(eligibilityOfMidterm!!)
+            else
+                Text(
+                    text = "No Date Provided",
+                )
         }
+        VerticalDivider(thickness = 5.dp)
         Column(
             modifier = Modifier
-                .fillMaxWidth()
-                .weight(1f),
+                .weight(1f)
+                .padding(5.dp)
         ) {
             Text(
-                modifier = Modifier
-                    .fillMaxWidth(),
-                text = "End Sem: $endSemEligibleText",
+                modifier = Modifier.fillMaxWidth(),
+                text = "End Sem",
                 textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Bold
-            )
-            if (!endSemDateNotProvided) {
-                if (eligibleMT == true) {
-                    Text(
-                        text = "Bunk Lectures: ${eligibilityOfEndSem!!.first.first}",
-                    )
-                }
-                Text(
-                    text = "More Lectures: ${eligibilityOfEndSem!!.first.second}",
+                style = MaterialTheme.typography.titleMedium.copy(
+                    fontWeight = FontWeight.Bold
                 )
-            }
+            )
+            if (endSemDateProvided)
+                EndSemText(eligibilityOfEndSem!!)
+            else
+                Text(
+                    text = "No Date Provided"
+                )
         }
+    }
+
+}
+
+@Composable
+private fun MidTermText(
+    eligibilityOfMidterm: EligibilityData
+) {
+    Text(
+        text = if (eligibilityOfMidterm.isEligibleForNow) "Eligible" else "Not Eligible"
+    )
+    if (eligibilityOfMidterm.isEligibleForNow)
+        Text(
+            text = "Can Bunk ${eligibilityOfMidterm.bunkLectures} lectures"
+        )
+    else if (eligibilityOfMidterm.isEligibleInFuture) {
+        Text(
+            text = "Eligible in Future If Attend ${eligibilityOfMidterm.moreLectures} lectures and bunk ${eligibilityOfMidterm.bunkLectures} lectures"
+        )
+
+    }
+}
+
+@Composable
+private fun EndSemText(
+    eligibilityOfEndSem: EligibilityData
+) {
+    Text(
+        text = if (eligibilityOfEndSem.isEligibleForNow) "Eligible" else "Not Eligible"
+    )
+    if (eligibilityOfEndSem.isEligibleForNow)
+        Text(
+            text = "Can Bunk ${eligibilityOfEndSem.bunkLectures} lectures"
+        )
+    else if (eligibilityOfEndSem.isEligibleInFuture) {
+        Text(
+            text = "Eligible in Future If Attend ${eligibilityOfEndSem.moreLectures} lecture and bunk ${eligibilityOfEndSem.bunkLectures} lectures"
+        )
     }
 }
