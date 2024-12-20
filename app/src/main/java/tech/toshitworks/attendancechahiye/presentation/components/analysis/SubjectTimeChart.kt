0aa060ce8f656b142396748d430d3a368932ae2c
toshit-dh <extratdh@gmail.com>
2024-12-20 10:42:23 +0530

    Added streak feature

--- app/src/main/java/tech/toshitworks/attendancechahiye/presentation/components/analysis/SubjectTimeChart.kt
+++ app/src/main/java/tech/toshitworks/attendancechahiye/presentation/components/analysis/SubjectTimeChart.kt
@@ -1,19 +1,26 @@
 package tech.toshitworks.attendancechahiye.presentation.components.analysis
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import co.yml.charts.common.model.PlotType
 import co.yml.charts.ui.piechart.charts.PieChart
@@ -97,6 +104,56 @@ fun SubjectTimeChart(
                         pieChartData,
                         pieChartConfig
                     )
+                else{
+                    Column (
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(8.dp),
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ){
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            text = "Streak",
+                            textAlign = TextAlign.Center,
+                            style = MaterialTheme.typography.titleMedium.copy(
+                                fontWeight = FontWeight.Bold
+                            )
+                        )
+                        Text(
+                            text = "Current:  5 🎯"
+                        )
+                        Text(
+                            text = "Longest:  8 🎯"
+                        )
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Center
+                        ) {
+                            Card(
+                                modifier = Modifier
+                                    .padding(6.dp)
+                                    .clickable {
+
+                                    },
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.primary,
+                                    contentColor = MaterialTheme.colorScheme.background
+                                ),
+                            ) {
+                                Column(
+                                    modifier = Modifier
+                                        .padding(6.dp)
+                                ) {
+                                    Text(
+                                        text = "View Streak"
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
     }
