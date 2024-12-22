ddac5d8f8a1653805cb1b715b4034e5b59c845f9
toshit-dh <extratdh@gmail.com>
2024-12-22 13:08:29 +0530

    removed unused imports

--- app/src/main/java/tech/toshitworks/attendancechahiye/presentation/components/dialogs/AddNoteDialog.kt
+++ app/src/main/java/tech/toshitworks/attendancechahiye/presentation/components/dialogs/AddNoteDialog.kt
@@ -7,8 +7,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 
 @Composable
 fun AddNoteDialog(
