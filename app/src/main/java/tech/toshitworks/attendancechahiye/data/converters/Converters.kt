c3a49fd8d884c92239fee347a645214b5d49c578
toshit-dh <extratdh@gmail.com>
2024-12-22 13:06:58 +0530

    removed println statement

--- app/src/main/java/tech/toshitworks/attendancechahiye/data/converters/Converters.kt
+++ app/src/main/java/tech/toshitworks/attendancechahiye/data/converters/Converters.kt
@@ -5,16 +5,11 @@ class Converters {
 
     @TypeConverter
     fun fromDatesListToString(forDates: List<String>): String {
-        println("List to be stored: $forDates")
         return forDates.joinToString(separator = ",")
     }
 
     @TypeConverter
     fun fromStringDatesToList(forDates: String?): List<String> {
-        println("String fetched from DB: $forDates")
-        val result = forDates?.split(",")?.filter { it.isNotBlank() } ?: emptyList()
-        println("Converted List: $result")
-        println(result.size)
-        return result
+        return forDates?.split(",")?.filter { it.isNotBlank() } ?: emptyList()
     }
 }
