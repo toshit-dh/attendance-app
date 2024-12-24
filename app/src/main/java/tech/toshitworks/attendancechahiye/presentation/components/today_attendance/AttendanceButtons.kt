c336f919e3b93a5222d8ecb95c4295a05fd718b0
toshit-dh <extratdh@gmail.com>
2024-12-24 23:59:23 +0530

    moved file

--- app/src/main/java/tech/toshitworks/attendancechahiye/presentation/components/today_attendance/AttendanceButtons.kt
+++ app/src/main/java/tech/toshitworks/attendancechahiye/presentation/components/today_attendance/AttendanceButtons.kt
@@ -1,4 +1,4 @@
-package tech.toshitworks.attendancechahiye.presentation.components.home
+package tech.toshitworks.attendancechahiye.presentation.components.today_attendance
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
