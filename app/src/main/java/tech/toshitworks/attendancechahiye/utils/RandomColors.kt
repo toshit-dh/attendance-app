bb4da02637bf62e0d3493e604df76201c5c7b76c
toshit-dh <extratdh@gmail.com>
2024-12-25 00:07:25 +0530

    moved colors to new file

---
+++ app/src/main/java/tech/toshitworks/attendancechahiye/utils/RandomColors.kt
@@ -0,0 +1,19 @@
+package tech.toshitworks.attendancechahiye.utils
+
+import androidx.compose.ui.graphics.Color
+
+
+val colors by lazy {
+    listOf(
+        Color(0xFFE57373),
+        Color(0xFF81C784),
+        Color(0xFF64B5F6),
+        Color(0xFFFFD54F),
+        Color(0xFFBA68C8),
+        Color(0xFFFF7043),
+        Color(0xFF4CAF50),
+        Color(0xFF9575CD),
+        Color(0xFFFFF176),
+        Color(0xFF29B6F6)
+    )
+}
