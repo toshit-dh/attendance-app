c1c038ce58cd118d7a41f352f3fc24ff52905577
toshit-dh <extratdh@gmail.com>
2024-12-25 00:09:34 +0530

    improved ui

--- app/src/main/java/tech/toshitworks/attendancechahiye/presentation/screen/today_attendance_screen/TodayAttendanceScreenUI.kt
+++ app/src/main/java/tech/toshitworks/attendancechahiye/presentation/screen/today_attendance_screen/TodayAttendanceScreenUI.kt
@@ -1,23 +1,11 @@
 package tech.toshitworks.attendancechahiye.presentation.screen.today_attendance_screen
 
-import CircularProgress
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import tech.toshitworks.attendancechahiye.presentation.components.home.TimetableForDay
+import tech.toshitworks.attendancechahiye.presentation.components.today_attendance.TimetableForDay
 import java.time.LocalDate
 
 
@@ -44,39 +32,6 @@ fun TodayAttendanceScreen(
         Column(
             modifier = modifier
         ) {
-            Card(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(165.dp)
-                    .padding(12.dp),
-            ) {
-                Text(
-                    text = "Overall Attendance",
-                    modifier = Modifier.padding(8.dp, 3.dp),
-                    style = MaterialTheme.typography.titleLarge.copy(
-                        fontSize = 24.sp,
-                        fontWeight = FontWeight.Bold,
-                        letterSpacing = 2.sp,
-                        color = MaterialTheme.colorScheme.onSurface
-                    )
-                )
-                Row(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        modifier = Modifier.weight(1f),
-                        text = "${state.attendanceStats.totalPresent} / ${state.attendanceStats.totalLectures}",
-                        fontSize = 20.sp
-                    )
-                    CircularProgress(
-                        modifier = Modifier.weight(1f),
-                        percentage = state.attendanceStats.attendancePercentage.toFloat()
-                    )
-                }
-            }
             val isValidDay = state.day != null && state.dayList.contains(state.day)
             val isInDateRange = tdLocale.isAfter(sdLocale) ||
                     (tdLocale.isEqual(sdLocale) && (endDateNull || tdLocale.isBefore(edLocale)))
