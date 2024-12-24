1dc4a2c7975eea40ef9cb7eac7ca8542a4b184d9
toshit-dh <extratdh@gmail.com>
2024-12-24 23:57:48 +0530

    improved ui

--- app/src/main/java/tech/toshitworks/attendancechahiye/presentation/components/analysis/AnalysisByTime.kt
+++ app/src/main/java/tech/toshitworks/attendancechahiye/presentation/components/analysis/AnalysisByTime.kt
@@ -4,13 +4,14 @@ package tech.toshitworks.attendancechahiye.presentation.components.analysis
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -34,85 +35,96 @@ fun AnalysisByTime(
     HorizontalPager(
         state = pagerState
     ) {
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp),
-            verticalArrangement = Arrangement.SpaceBetween
-        ) {
-            when (it) {
-                0 -> {
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        text = "Lectures By Day",
-                        style = MaterialTheme.typography.titleLarge.copy(
-                            fontWeight = FontWeight.Bold
-                        ),
-                        textAlign = TextAlign.Center
-                    )
-                    Spacer(Modifier.height(4.dp))
-                    analyticsByDay.forEach {
-                        Row(
+        Card {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(8.dp),
+                verticalArrangement = Arrangement.spacedBy(6.dp)
+            ) {
+                when (it) {
+                    0 -> {
+                        Text(
                             modifier = Modifier
                                 .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween
-                        ){
-                            Text(text = it.day.name)
-                            Text(
-                                text = "${it.lecturesPresent} / ${it.lecturesConducted}"
-                            )
-                        }
-
-                    }
-                }
-
-                1 -> {
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        text = "Lectures By Week",
-                        style = MaterialTheme.typography.titleLarge.copy(
-                            fontWeight = FontWeight.Bold
-                        ),
-                        textAlign = TextAlign.Center
-                    )
-                    Spacer(Modifier.height(4.dp))
-                    analyticsByWeek.forEach {
-                        Row(
-                            modifier = Modifier
-                                .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween
+                            text = "Lectures By Day",
+                            style = MaterialTheme.typography.titleLarge.copy(
+                                fontWeight = FontWeight.Bold
+                            ),
+                            textAlign = TextAlign.Center
+                        )
+                        LazyColumn(
+                            verticalArrangement = Arrangement.spacedBy(6.dp)
                         ) {
-                            Text(text = it.yearWeek)
-                            Text(
-                                text = "${it.lecturesPresent} / ${it.lecturesConducted}"
-                            )
+                            items(analyticsByDay) {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Text(text = it.day.name)
+                                    Text(
+                                        text = "${it.lecturesPresent} / ${it.lecturesConducted}"
+                                    )
+                                }
+                            }
+
                         }
                     }
-                }
 
-                2 -> {
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        text = "Lectures By Month",
-                        style = MaterialTheme.typography.titleLarge.copy(
-                            fontWeight = FontWeight.Bold
-                        ),
-                        textAlign = TextAlign.Center
-                    )
-                    Spacer(Modifier.height(4.dp))
-                    analyticsByMonth.forEach {
-                        Row(
+                    1 -> {
+                        Text(
                             modifier = Modifier
                                 .fillMaxWidth(),
-                            horizontalArrangement = Arrangement.SpaceBetween
+                            text = "Lectures By Week",
+                            style = MaterialTheme.typography.titleLarge.copy(
+                                fontWeight = FontWeight.Bold
+                            ),
+                            textAlign = TextAlign.Center
+                        )
+                        LazyColumn(
+                            verticalArrangement = Arrangement.spacedBy(6.dp)
                         ) {
-                            Text(text = it.yearMonth)
-                            Text(
-                                text = "${it.lecturesPresent} / ${it.lecturesConducted}"
-                            )
+                            items(analyticsByWeek) {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Text(text = it.yearWeek)
+                                    Text(
+                                        text = "${it.lecturesPresent} / ${it.lecturesConducted}"
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+                    2 -> {
+                        Text(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            text = "Lectures By Month",
+                            style = MaterialTheme.typography.titleLarge.copy(
+                                fontWeight = FontWeight.Bold
+                            ),
+                            textAlign = TextAlign.Center
+                        )
+                        LazyColumn(
+                            verticalArrangement = Arrangement.spacedBy(6.dp)
+                        ) {
+                            items(analyticsByMonth) {
+                                Row(
+                                    modifier = Modifier
+                                        .fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.SpaceBetween
+                                ) {
+                                    Text(text = it.yearMonth)
+                                    Text(
+                                        text = "${it.lecturesPresent} / ${it.lecturesConducted}"
+                                    )
+                                }
+                            }
                         }
                     }
                 }
