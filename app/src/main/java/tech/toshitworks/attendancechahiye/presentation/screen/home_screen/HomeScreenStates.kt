0e91e7221db235590b35374f17b0de5c815a913c
toshit-dh <extratdh@gmail.com>
2024-12-29 15:29:49 +0530

    EditInfoScreen setup

--- app/src/main/java/tech/toshitworks/attendancechahiye/presentation/screen/home_screen/HomeScreenStates.kt
+++ app/src/main/java/tech/toshitworks/attendancechahiye/presentation/screen/home_screen/HomeScreenStates.kt
@@ -2,6 +2,7 @@ package tech.toshitworks.attendancechahiye.presentation.screen.home_screen
 
 import tech.toshitworks.attendancechahiye.domain.model.AttendanceBySubject
 import tech.toshitworks.attendancechahiye.domain.model.AttendanceStats
+import tech.toshitworks.attendancechahiye.domain.model.DayModel
 import tech.toshitworks.attendancechahiye.domain.model.SubjectModel
 
 data class HomeScreenStates(
@@ -11,10 +12,17 @@ data class HomeScreenStates(
     val isSubjectSearchOpen: Boolean = false,
     val attendanceStats: AttendanceStats? = null,
     val attendanceBySubject: List<AttendanceBySubject> = emptyList(),
+    val dayList: List<DayModel> = emptyList(),
     val todayDate: String = "",
     val todayDay: String = "",
     val editAttendanceDate: String? = null,
     val subjectList: List<SubjectModel> = emptyList(),
     val analysisSubject: SubjectModel? = null,
+    val editList: List<String> = listOf(
+        "Semester",
+        "Subject",
+        "Timetable"
+    ),
+    val editInfo: Int = 0,
     val csvWorkerState: String = ""
 )
