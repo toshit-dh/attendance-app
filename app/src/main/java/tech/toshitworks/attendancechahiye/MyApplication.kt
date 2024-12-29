82b5d9dc6705cdb4b3c1c51d2baee8cc1f61c660
toshit-dh <extratdh@gmail.com>
2024-12-29 15:36:19 +0530

    added notification channel code

--- app/src/main/java/tech/toshitworks/attendancechahiye/MyApplication.kt
+++ app/src/main/java/tech/toshitworks/attendancechahiye/MyApplication.kt
@@ -1,7 +1,25 @@
 package tech.toshitworks.attendancechahiye
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication: Application()
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        val name = getString(R.string.notification_channel)
+        val descriptionText = getString(R.string.notification_description)
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(getString(R.string.channel_id), name, importance).apply {
+            description = descriptionText
+        }
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+    }
+}
