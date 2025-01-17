e738e372667cfd5a00e94e689d12cf02b7087271
toshit-dh <extratdh@gmail.com>
2025-01-17 22:05:10 +0530

    added feature to edit subject

--- app/src/main/java/tech/toshitworks/attendo/presentation/screen/form_screen/FormScreenEvents.kt
+++ app/src/main/java/tech/toshitworks/attendo/presentation/screen/form_screen/FormScreenEvents.kt
@@ -4,6 +4,7 @@ import tech.toshitworks.attendo.domain.model.SubjectModel
 sealed class FormScreenEvents {
     data class OnDayClick(val name: String,val add: Boolean) : FormScreenEvents()
     data class OnAddSubjectClick(val subjectModel: SubjectModel): FormScreenEvents()
+    data class OnEditSubjectClick(val subjectModel: SubjectModel,val index: Int): FormScreenEvents()
     data class OnRemoveSubjectClick(val subjectModel: SubjectModel): FormScreenEvents()
     data class OnAddPeriodClick(val startTime: String,val endTime: String,val periodDuration: Int): FormScreenEvents()
     data object AddInRoom: FormScreenEvents()
