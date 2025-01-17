e4a1552df15cd77346e0b3adcd2f949fbb001780
toshit-dh <extratdh@gmail.com>
2025-01-17 22:00:12 +0530

    added delete screen

--- app/src/main/java/tech/toshitworks/attendo/navigation/ScreenRoutes.kt
+++ app/src/main/java/tech/toshitworks/attendo/navigation/ScreenRoutes.kt
@@ -2,6 +2,7 @@ package tech.toshitworks.attendo.navigation
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Analytics
+import androidx.compose.material.icons.filled.DeleteSweep
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.EditNote
 import androidx.compose.material.icons.filled.Email
@@ -21,13 +22,15 @@ sealed class ScreenRoutes(val route: String)  {
     data object TimetableScreen: ScreenRoutes("attendance_screen")
     data object TodayAttendance: ScreenRoutes("today_attendance_screen")
     data object AnalyticsScreen: ScreenRoutes("analytics_screen")
+    data object EditAttendanceScreen : ScreenRoutes("edit_attendance_screen")
     data object EditInfoScreen: ScreenRoutes("edit_info_screen")
     data object EventsScreen: ScreenRoutes("events_screen")
     data object NotesScreen : ScreenRoutes("notes_screen")
     data object ExportScreen : ScreenRoutes("export_screen")
     data object SettingsScreen : ScreenRoutes("settings_screen")
+    data object DeleteScreen : ScreenRoutes("delete_screen")
     data object NotificationScreen : ScreenRoutes("notification_screen")
-    data object EditAttendanceScreen : ScreenRoutes("edit_attendance_screen")
+
 }
 enum class DrawerScreens(
     val route: String,
@@ -38,7 +41,8 @@ enum class DrawerScreens(
     Notes(ScreenRoutes.NotesScreen.route,"Notes",Icons.Filled.Email),
     Events(ScreenRoutes.EventsScreen.route,"Events",Icons.Filled.EventRepeat),
     Export(ScreenRoutes.ExportScreen.route,"Export",Icons.Filled.FileOpen),
-    Settings(ScreenRoutes.SettingsScreen.route,"Settings",Icons.Filled.Settings)
+    Settings(ScreenRoutes.SettingsScreen.route,"Settings",Icons.Filled.Settings),
+    Delete(ScreenRoutes.DeleteScreen.route,"Delete Data",Icons.Filled.DeleteSweep)
 }
 enum class BottomScreens(
     val route: String,
