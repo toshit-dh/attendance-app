1573051500a4c6e1ceb268392273885bf5760d23
toshit-dh <extratdh@gmail.com>
2025-01-17 22:05:35 +0530

    changed date parameter type

--- app/src/main/java/tech/toshitworks/attendo/presentation/screen/form_screen/FormScreen1Events.kt
+++ app/src/main/java/tech/toshitworks/attendo/presentation/screen/form_screen/FormScreen1Events.kt
@@ -3,6 +3,6 @@ package tech.toshitworks.attendo.presentation.screen.form_screen
 sealed class FormScreen1Events {
         data class OnSemesterNumberChange(val semNumber: Int) : FormScreen1Events()
         data class OnStartDateChange(val startDate: String) : FormScreen1Events()
-        data class OnMidTermDateChange(val midTermDate: String) : FormScreen1Events()
-        data class OnEndDateChange(val endDate: String) : FormScreen1Events()
+        data class OnMidTermDateChange(val midTermDate: String?) : FormScreen1Events()
+        data class OnEndDateChange(val endDate: String?) : FormScreen1Events()
 }
